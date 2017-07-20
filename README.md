75d73b94e7bd187a8a97cabd969d4c90c9950ca4
JaeHo Song <thruthesky@gmail.com>
2017-07-20 19:31:07 +0800

    update for facebook auto loader

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# auto-post-for-william
+# Facebook Auto Posting
+
+This is for william go took the money from me.
+
+
+# Configuration
+
+create config.ts and put like below.
+
+````
+export const config = {
+    postInWall: false,                   /// to post on the wall of the account.
+    facebookAccount: {
+        id: 'thruthesky@hanmail.net',
+        passwrod: '******'
+    },
+    groups: [                           /// groups to post. 주의: m 모바일 사이트라야 한다.
+        'https://m.facebook.com/groups/ontue/',
+        'https://m.facebook.com/groups/phillove/'
+    ]
+};
+
+
+````
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-
+/**
+ * @see README
+ */
 import * as Nightmare from 'nightmare';
-import { FacebookAccount } from './config';
-
-
+import { config } from './config';
 class AutoPost {
 
     nightmare: Nightmare;
@@ -14,10 +14,13 @@ class AutoPost {
             typeInterval: 20
         });
 
+
+        this.resetNightmare();
+    }
+    resetNightmare() {
         /// 아래 User Agent 로 접속하면, Firefox 모드로 접속한다.
         /// 이렇게 하면 Site Preview 와 Youtube Preview 가 된다.
         this.nightmare.useragent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:54.0) Gecko/20100101 Firefox/54.0");
-
     }
 
     async run() {
@@ -25,26 +28,45 @@ class AutoPost {
         console.log("finished");
     }
 
+    async post(url) {
+        return await this.nightmare
+            .goto(url)
+            .wait(2000)
+            .wait('textarea')
+            .wait(1000)
+            .type('textarea', 'http://www.philgo.com/?1273287271')
+            .wait(500)
+            .click('[name="view_post"]')
+            .wait(1000)
+            .evaluate(() => {
+
+            })
+            .then(() => {
+                console.log('url: ', url);
+            });
+    }
+
     async openMFacebook() {
         /// 모바일 버전의 Facebook 에서 Firefox 로 접속 할 때, Site Preview 가 된다.
         await this.nightmare.goto("https://m.facebook.com")
             .wait('[name="email"]')
-            .type('[name="email"]', FacebookAccount.id)
-            .type('[name="pass"]', FacebookAccount.passwrod)
+            .type('[name="email"]', config.facebookAccount.id)
+            .type('[name="pass"]', config.facebookAccount.passwrod)
             .click('[name="login"]')
-            .wait(2000)
-            .goto('https://m.facebook.com/stories.php')
-            .wait('textarea')
-            .wait(1000)
-            .type('textarea', 'https://www.philgo.com/?1273287271')
-            .wait(500)
-            .click('[name="view_post"]')
-            .evaluate(() => {
-                
-            })
-            .then((body) => {
-                console.log(body)
-            });
+            .wait('img[alt*="Nam"]')
+            .evaluate(() => { })
+            .then(() => { });
+
+
+        if (config.postInWall) {
+            await this.post('https://m.facebook.com/stories.php');
+        }
+
+        if (config.groups && config.groups.length) {
+            for (let groupUrl of config.groups) {
+                await this.post( groupUrl );
+            }
+        }
     }
 }
 
